f0a214a38aaf195d4e0d8b760175ba117e25875d
ZwEin <zwein27@gmail.com>
2016-06-27 16:07:59 -0700

    0.1.1 version

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from setuptools import Extension,find_packages
+from os import path
+
+setup(
+    name = 'pylabelprop',
+    version = '0.1.1',
+    description = 'Python implementation of label propagation',
+    author = 'Lingzhe Teng',
+    author_email = 'zwein27@gmail.com',
+    url = 'https://github.com/ZwEin27/python-labelpropagation',
+    download_url = 'https://github.com/ZwEin27/python-labelpropagation/tarball/0.1.1',
+    packages = find_packages(),
+    keywords = ['label propagation', 'label', 'propagation', 'labelpropagation']
+)
