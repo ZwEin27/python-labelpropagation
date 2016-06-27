0c2bf247341d7250a96a244c10fd529736ab0491
ZwEin <zwein27@gmail.com>
2016-06-27 12:26:59 -0700

    add data loader

--- labelprop.py
+++ labelprop.py
@@ -2,8 +2,10 @@
 # @Author: ZwEin
 # @Date:   2016-06-24 14:56:40
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-06-27 11:11:13
+# @Last Modified time: 2016-06-27 12:26:34
 
+import ast
+import heapq
 
 class Edge():
     def __init__(self, src, dest, weight):
@@ -13,9 +15,14 @@ class Edge():
 
 
 
+
+
 class LabelProp():
 
-    def __init__(self, steps=10):
+    def __init__(self):
+        self.initialize_env()
+
+    def initialize_env(self):
         self.vertex_adj_map = {}         # int: [Edge]
         self.vertex_in_adj_map = {}      # int: float
         self.vertex_deg_map = {}         # int, float
@@ -25,8 +32,100 @@ class LabelProp():
         self.vertex_size = 0
         self.label_size = 0
         self.labelled_size = 0
+        
 
-        self.steps = steps
+    def setup_env(self):
+
+        # initialize vertex_in_adj_map
+        for vertex_id in self.vertex_adj_map.keys():
+            if vertex_id not in self.vertex_in_adj_map:
+                self.vertex_in_adj_map.setdefault(vertex_id, [])
+
+        # setup vertex_in_adj_map
+        for vertex_id in self.vertex_in_adj_map.keys():
+            for edge in self.vertex_adj_map[vertex_id]:
+                self.vertex_in_adj_map[edge.dest].append(edge)
+
+        # setup vertex_deg_map
+        for vertex_id in self.vertex_adj_map.keys():
+            degree = .0
+            if vertex_id in self.vertex_deg_map:
+                degree = self.vertex_deg_map[vertex_id]
+            for edge in self.vertex_adj_map[vertex_id]:
+                degree += edge.weight
+            self.vertex_deg_map[vertex_id] = degree
+
+        # setup vertex_f_map
+        v_set = self.vertex_label_map.keys()
+        l_set = []
+        for v in v_set:
+            l = self.vertex_label_map[v]
+            heapq.heappush(l_set, l)
+            self.vertex_size += 1
+
+        label_enum = 0
+        for l in heapq.nsmallest(len(l_set), l_set):
+            if int(l) == 0:
+                continue
+            self.label_index_map[l] = label_enum
+            label_enum += 1
+        self.label_size = label_enum
+
+        labelled_size = 0
+        for v in v_set:
+            arr = []
+            l = int(self.vertex_label_map[v])
+            if l == 0:
+                # unlabelled
+                for i in range(label_enum):
+                    arr.append(.0)
+            else:
+                # labelled
+                labelled_size += 1
+                ix = int(self.label_index_map[self.vertex_label_map[v]])
+                for i in range(label_enum):
+                    if i == ix:
+                        arr.append(1.)
+                    else:
+                        arr.append(0.)
+            self.vertex_f_map[v] = arr
+
+
+    def load_data_from_file(self, filename):
+        with open(filename, 'rb') as f:
+            lines = f.readlines()
+            self.load_data_from_mem(lines)
+
+    def load_data_from_mem(self, data):
+        for line in data:
+            line = line.strip()
+            self.process_data_line(line)
+        self.setup_env()
+
+    def process_data_line(self, dataline):
+        # [vertexId, vertexLabel, [edges]]
+        # unlabeled vertex if vertexLabel == 0
+        # i.e. [2, 1, [[1, 1.0], [3, 1.0]]]
+        
+        try:
+
+            line = ast.literal_eval(dataline)
+            vertex_id = line[0]
+            vertex_label = line[1]
+            edges = line[2]
+            edge_list = []
+            self.vertex_label_map.setdefault(vertex_id, vertex_label)
+            for edge in edges:
+                dest_vertex_id = int(edge[0])
+                edge_weight = float(edge[1])
+                edge_list.append(Edge(vertex_id, dest_vertex_id, edge_weight))
+            self.vertex_adj_map.setdefault(vertex_id, edge_list)
+
+        except Exception as e:
+
+            raise Exception("Coundn't parse vertex from line: ", line, e)
+
+        
 
     def show_detail(self):
         print "Number of vertices:            ", self.vertex_size
@@ -120,7 +219,9 @@ class LabelProp():
 
 if __name__ == '__main__':
     labelprop = LabelProp()
-    
+    labelprop.load_data_from_file('data/sample.json')
+
+
 
 
 
